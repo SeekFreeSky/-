71bdb9bb8613209335f26f0cf3b1cc61a3d229bb
SeekFreeSky <124674257+SeekFreeSky@users.noreply.github.com>
2026-01-22 17:13:37 +0800

    Update HaoKanSearch.user.js

--- HaoKanSearch.user.js
+++ HaoKanSearch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         好看视频标题搜索 (完美版-防丢失+可配置)
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/SeekFreeSky/HaoKanSearch
 // @version      0.0.6
 // @description  在好看视频网页中添加按钮：支持配置搜索引擎、边界自动吸附、防丢失、触屏拖拽。
 // @author       SeekFreeSky
